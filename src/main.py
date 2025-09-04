528b41dd4f358a6b3dbc355976b938f4e6d81dd4
Cornelias35 <kirtan_bedirhan@hotmail.com>
2025-09-04 22:57:15 +0300

    .env loading

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
 from models.train import train_model
+import os
+from dotenv import load_dotenv
+import logging
 
 if __name__ == "__main__":
+    try:
+        load_dotenv(dotenv_path="../.env")
+        os.environ["WANDB_API_KEY"] = os.getenv("WANDB_API_KEY")
+    except FileNotFoundError:
+        logging.error('Could not find WANDB_API_KEY')
+        raise FileNotFoundError
+
     train_model()
